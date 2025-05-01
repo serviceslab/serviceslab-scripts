79e0e8217d33e6a6fa86da25e0f9c397392406c6
code7a <115498258+code7a@users.noreply.github.com>
2025-04-30 22:09:57 -0400

    get activation code update

--- get-pce-activation-code.sh
+++ get-pce-activation-code.sh
@@ -2,10 +2,10 @@
 yum install -y jq
 #port=$(cat /etc/illumio-pce/runtime_env.yml | grep front_end_https_port | cut -d' ' -f2)
 basic_auth_token=$(echo -n "$pce_admin_username_email_address:$pce_admin_password"|base64 --wrap=0)
-auth_token=$(curl -X POST -H "Authorization: Basic $basic_auth_token" https://$(hostname):$port/api/v2/login_users/authenticate?pce_fqdn=$(hostname) | jq -r '.auth_token')
+auth_token=$(curl -X POST -H "Authorization: Basic $basic_auth_token" https://$fqdn:$port/api/v2/login_users/authenticate?pce_fqdn=$fqdn | jq -r '.auth_token')
 login_response=$(curl -H "Authorization: Token token=$auth_token" https://$(hostname):$port/api/v2/users/login)
 auth_username=$(echo $login_response | jq -r '.auth_username')
 session_token=$(echo $login_response | jq -r '.session_token')
 #get activation code
-activation_code=$(curl -u $auth_username:$session_token https://$(hostname):$port/api/v2/orgs/1/pairing_profiles/1/pairing_key -X POST -H 'content-type: application/json' --data-raw '{}' | jq -r .activation_code)
+activation_code=$(curl -u $auth_username:$session_token https://$fqdn:$port/api/v2/orgs/1/pairing_profiles/1/pairing_key -X POST -H 'content-type: application/json' --data-raw '{}' | jq -r .activation_code)
 export activation_code
