3bec460be70c3c2961b2f004c18f0eb6eec25639
code7a <115498258+code7a@users.noreply.github.com>
2025-04-30 22:47:21 -0400

    get activation code update

--- get-pce-activation-code.sh
+++ get-pce-activation-code.sh
@@ -3,7 +3,7 @@ yum install -y jq || true
 #port=$(cat /etc/illumio-pce/runtime_env.yml | grep front_end_https_port | cut -d' ' -f2)
 basic_auth_token=$(echo -n "$pce_admin_username_email_address:$pce_admin_password"|base64 --wrap=0)
 auth_token=$(curl -X POST -H "Authorization: Basic $basic_auth_token" https://$fqdn:$port/api/v2/login_users/authenticate?pce_fqdn=$fqdn | jq -r '.auth_token')
-login_response=$(curl -H "Authorization: Token token=$auth_token" https://$(hostname):$port/api/v2/users/login)
+login_response=$(curl -H "Authorization: Token token=$auth_token" https://$fqdn:$port/api/v2/users/login)
 auth_username=$(echo $login_response | jq -r '.auth_username')
 session_token=$(echo $login_response | jq -r '.session_token')
 #get activation code
