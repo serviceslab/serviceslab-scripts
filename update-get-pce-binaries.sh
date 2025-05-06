9e28dad7fa2aeeed216ba2af39f026f59b173f3a
code7a <115498258+code7a@users.noreply.github.com>
2025-05-06 08:42:58 -0400

    background curl gets

--- update-get-pce-binaries.sh
+++ update-get-pce-binaries.sh
@@ -101,7 +101,7 @@ for major_minor_version in $major_minor_list; do
         fi
 
         cat <<EOF >> "$temp_target_script"
-  curl --remote-name https://$repo/$major_minor_version/GA%20Releases/$patch/pce/pkgs/$pce_pkg
+  curl --remote-name https://$repo/$major_minor_version/GA%20Releases/$patch/pce/pkgs/$pce_pkg &
   curl --remote-name https://$repo/$major_minor_version/GA%20Releases/$patch/pce/pkgs/UI/$ui_pkg
   curl --remote-name $compat_url &
 EOF
