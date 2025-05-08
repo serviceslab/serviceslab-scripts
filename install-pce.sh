0d507b79e059d0ba9523920bb0e6afbe0385fd74
code7a <115498258+code7a@users.noreply.github.com>
2025-05-08 15:05:08 -0400

    install from tmp

--- install-pce.sh
+++ install-pce.sh
@@ -2,7 +2,7 @@
 yum install -y wget bzip2 net-tools initscripts libxcrypt-compat compat-openssl11 glibc-langpack-en
 localectl set-locale LANG=en_US.utf8
 
-rpm -Uvh /illumio-pce-*.rpm
+rpm -Uvh /tmp/illumio-pce-*.rpm
 #mkdir /opt/illumio-pce/ && tar -xf illumio-pce-*.tgz -C /opt/illumio-pce/ && chown -R root:ilo-pce /opt/illumio-pce/
 
 #file kernal settings
@@ -28,13 +28,13 @@ sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl status -w
 #create domain
 sudo --preserve-env -u ilo-pce ILO_PASSWORD="$pce_admin_password" /opt/illumio-pce/illumio-pce-db-management create-domain --user-name $pce_admin_username_email_address --full-name admin --org-name $(hostname)
 #get latest ven bundle
-latest_illumio_ven_bundle=$(ls /illumio-ven-bundle-*|tail -n 1)
+latest_illumio_ven_bundle=$(ls /tmp/illumio-ven-bundle-*|tail -n 1)
 #install ven bundle
-sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl ven-software-install $latest_illumio_ven_bundle --compatibility-matrix /illumio-release-compatibility-* --default --no-prompt --orgs 1
-illumio_ven_bundles=($(ls /illumio-ven-bundle-*))
+sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl ven-software-install $latest_illumio_ven_bundle --compatibility-matrix /tmp/illumio-release-compatibility-* --default --no-prompt --orgs 1
+illumio_ven_bundles=($(ls /tmp/illumio-ven-bundle-*))
 for illumio_ven_bundle in "${illumio_ven_bundles[@]}"; do
  if [[ $illumio_ven_bundle == $latest_illumio_ven_bundle ]]; then continue; fi
- sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl ven-software-install $illumio_ven_bundle --compatibility-matrix /illumio-release-compatibility-* --no-prompt --orgs 1
+ sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl ven-software-install $illumio_ven_bundle --compatibility-matrix /tmp/illumio-release-compatibility-* --no-prompt --orgs 1
 done
 #sleep 10 && systemctl restart sshd &
 #pkill sshd &
