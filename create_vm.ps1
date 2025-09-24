28bcafcae1f523672924a2644a611bfe5551c5fd
code7a <115498258+code7a@users.noreply.github.com>
2025-09-23 22:22:52 -0400

    datastore change

--- create_vm.ps1
+++ create_vm.ps1
@@ -5,18 +5,24 @@
 Set-PowerCLIConfiguration -WebOperationTimeoutSeconds 1800 -Scope Session -Confirm:$false
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 Connect-VIServer -Server $hypervisor_host -User $hypervisor_user -Password $hypervisor_password
-$vm_location=Get-Folder $vm_folder -ErrorAction SilentlyContinue -ErrorVariable NoFolder | Where-Object {$_.Parent.Name -eq $vm_parent_folder}
+$vm_location=Get-Folder $vm_folder -ErrorAction SilentlyContinue -ErrorVariable NoFolder | Where-Object{$_.Parent.Name -eq $vm_parent_folder}
 $vm_template_obj=Get-Template $vm_template
 $vm_hostname="$hostname_prefix.$app.$domain"
 Write-Host "$vm_hostname"
 Write-Host "Launching VM..."
-$vm_host = Get-VMHost -State Connected | Get-Random
-if ($vm_host -eq "172.22.91.13") { $vm_host = "172.22.91.12" }
-if ($vm_host -eq "172.22.91.12") {
-    $vm=New-VM -Template $vm_template_obj -Name $vm_hostname -Location (Get-Folder -Id $vm_location.Id) -VMHost $vm_host -Datastore (Get-Datastore -RelatedObject $vm_host | Where-Object { $_.Name -like "*local*" } | Get-Random)
+$vm_host=Get-VMHost -State Connected | Get-Random
+if($vm_host -eq "172.22.91.13"){$vm_host="172.22.91.12"}
+if($vm_host -eq "172.22.91.12"){
+    Write-Host $vm_host
+    $datastore=Get-Datastore -RelatedObject $vm_host | Where-Object{$_.Name -like "*local*"} | Get-Random
+    Write-Host $datastore
+    $vm=New-VM -Template $vm_template_obj -Name $vm_hostname -Location(Get-Folder -Id $vm_location.Id) -VMHost $vm_host -Datastore $datastore
 }
-else {
-    $vm=New-VM -Template $vm_template_obj -Name $vm_hostname -Location (Get-Folder -Id $vm_location.Id) -VMHost $vm_host -Datastore (Get-Datastore -RelatedObject $vm_host | Where-Object { $_.Name -like "*support*" } | Get-Random)
+else{
+    Write-Host $vm_host
+    $datastore=Get-Datastore -RelatedObject $vm_host | Where-Object{$_.Name -like "*support*"} | Get-Random
+    Write-Host $datastore
+    $vm=New-VM -Template $vm_template_obj -Name $vm_hostname -Location(Get-Folder -Id $vm_location.Id) -VMHost $vm_host -Datastore $datastore
 }
 Write-Host "VM created."
 Start-Sleep 20
@@ -25,8 +31,8 @@ Start-Sleep 10
 Write-Host "Starting VM..."
 Start-VM $vm
 #while loop until boots and gets an ip
-while ($null -eq $vm_ip){
-    $vm_ip = Get-VM $vm | ForEach-Object{$_.Guest.IPAddress} | Where-Object{$_ -like "*.*"}
+while($null -eq $vm_ip){
+    $vm_ip=Get-VM $vm | ForEach-Object{$_.Guest.IPAddress} | Where-Object{$_ -like "*.*"}
     Start-Sleep 10}
 Write-Host "VM started."
 Write-Output $vm_ip > "$($ENV:BUILD_TAG)-vm_ip.txt"
