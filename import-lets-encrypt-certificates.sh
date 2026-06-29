469036398f22fc132c95b2dd99ed6bdd5cd1bad1
code7a <115498258+code7a@users.noreply.github.com>
2026-06-29 16:51:32 -0400

    updated cert

--- import-lets-encrypt-certificates.sh
+++ import-lets-encrypt-certificates.sh
@@ -2,8 +2,8 @@
 cd /etc/pki/ca-trust/source/anchors/
 curl -O https://letsencrypt.org/certs/isrgrootx1.pem
 curl -O https://letsencrypt.org/certs/isrg-root-x2.pem
-curl -O https://letsencrypt.org/certs/gen-y/root-ye.pem
-curl -O https://letsencrypt.org/certs/gen-y/root-yr.pem
+curl -O https://letsencrypt.org/certs/gen-y/root-ye-by-x2.pem
+curl -O https://letsencrypt.org/certs/gen-y/root-yr-by-x1.pem
 curl -O https://letsencrypt.org/certs/2024/e7.pem
 curl -O https://letsencrypt.org/certs/2024/e8.pem
 curl -O https://letsencrypt.org/certs/2024/r12.pem
